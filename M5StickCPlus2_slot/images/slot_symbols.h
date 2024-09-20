5f175541487a8b3d092c5c135a3000bc898a7f7b
OpenELAB <info@openelab.io>
2024-09-20 11:55:13 +0800

    Add files via upload

---
+++ M5StickCPlus2_slot/images/slot_symbols.h
@@ -0,0 +1,19 @@
+#include "kiss.h"
+#include "Happy.h"
+#include "Spectre.h"
+#include "Ghostface.h"
+#include "Think.h"
+#include "Vomit.h"
+
+#define SYM_WIDTH 48
+#define SYM_HEIGHT 48
+#define SYM_COUNT 6
+
+const uint16_t *slot_symbols[] = {
+	Kiss,
+	Happy,
+	Spectre,
+	Ghostface,
+	Think,
+	Vomit,
+};
